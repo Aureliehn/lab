176c621938a7a2053fe04f30a18ff623a54af8f0
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-03-11 12:49:39 +0100

    test

--- leLab/leLab/leLab/Views/trainig/Page2.xaml.cs
+++ leLab/leLab/leLab/Views/trainig/Page2.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using leLab.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,13 @@ namespace leLab.Views.trainig
         public Page2()
         {
             InitializeComponent();
+            label_out_operation.IsVisible = true;
 
             vehicules = new List<Vehicules>();
-            vehicules.Add(new Vehicules{ Titre = "VSAV", Cod = "Disponible", Name = "Disponibe", Available = false, NbrPompier = 3 , Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg"});
-            vehicules.Add(new Vehicules { Titre = "VSAV", Cod = "Sur les lieux", Name = "Sur les lieux", Available = true, NbrPompier = 3, Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg" });
-            vehicules.Add(new Vehicules { Titre = "VSAV", Cod = "En route", Name = "En route", Available = true, NbrPompier = 3, Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg" });
-            vehicules.Add(new Vehicules { Titre = "CCFM", Cod = "Disponible radio", Name = "Disponibe radio", Available = false, NbrPompier = 4, Image = "https://www.desautel-firetrucks.com/wp-content/uploads/2020/09/presentation-ccfm-768x0-c-default.jpg" });
+            vehicules.Add(new Vehicules{ Titre = "VSAV", Cod = "Disponible", Name = "Disponibe", Available = true, NbrPompier = 3 , Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg", LabelIsVisible = false});
+            vehicules.Add(new Vehicules { Titre = "VSAV", Cod = "Sur les lieux", Name = "Sur les lieux", Available = true, NbrPompier = 3, Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg", LabelIsVisible = true });
+            vehicules.Add(new Vehicules { Titre = "VSAV", Cod = "En route", Name = "En route", Available = true, NbrPompier = 3, Image = "https://vehicule-pompiers.fr/fr/wp-content/uploads/2020/03/vehicule-pompiers-renault-master-orion-vsav-2020.jpg",LabelIsVisible = true });
+            vehicules.Add(new Vehicules { Titre = "CCFM", Cod = "Disponible radio", Name = "Disponibe radio", Available = true, NbrPompier = 4, Image = "https://www.desautel-firetrucks.com/wp-content/uploads/2020/09/presentation-ccfm-768x0-c-default.jpg" , LabelIsVisible = false });
             
             listView.ItemsSource = vehicules;
 
@@ -45,8 +47,39 @@ namespace leLab.Views.trainig
                 }
             }
             Console.WriteLine(v2);
+            if(v2.Count == 0)
+            {
+                label_out_operation.IsVisible = false;
+            }
+
             return v2;
-        }
+                    
 
     }
+        public new bool IsVisible
+        {
+            get { return true; }
+        }
+            /*if(v2.Count > 0)
+            {
+                this.IsVisible = false;
+            }*/
+
+
+
+
+
+
+    /*      public bool IsVisible
+    {
+        get { return _pin.IsCalloutVisible(); }
+    }
+            public bool IsCalloutVisible()
+    {
+        return _mapView != null ? _mapView.IsCalloutVisible(_callout) : false;
+    }
+
+     */
+
+}
 }
